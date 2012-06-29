26811af8e57b3fdf03a7b028b649bc750f631151
Peter Ritchie <github.com@peterRitchie.com>
2012-06-29 10:15:41 -0400

    added template-generated code

---
+++ Service/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceProcess;
+
+namespace Service
+{
+	static class Program
+	{
+		static void Main()
+		{
+			ServiceBase[] ServicesToRun;
+			ServicesToRun = new ServiceBase[] 
+			{ 
+				new Service() 
+			};
+			ServiceBase.Run(ServicesToRun);
+		}
+	}
+}
